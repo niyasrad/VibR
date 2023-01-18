23e1a954c289ba7584229bb99d73320063b83cc6
Niyas Hameed <84234554+niyasrad@users.noreply.github.com>
2023-01-19 03:26:24 +0530

    [Niyas] CallBack Fix

--- src/component/card/Card.tsx
+++ src/component/card/Card.tsx
@@ -35,7 +35,7 @@ const Card = forwardRef((props: cardProps, ref: any) => {
                     props.items.data.items.slice(0, props.limit).map((s: any) => (
                     <SmallCard artist={s.album.artists[0].name}  title={s.name} img={s.album.images[1]['url']}/>
                 ))
-                :db.map(s => (
+                :db.slice(0, props.limit).map(s => (
                     <SmallCard artist={s.artist} title={s.title} img={s.img} />
                 )) }
             </div>
